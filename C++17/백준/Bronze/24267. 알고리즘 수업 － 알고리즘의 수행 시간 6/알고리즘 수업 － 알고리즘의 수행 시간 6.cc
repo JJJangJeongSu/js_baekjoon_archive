0a5fbd5ee69a18cbe12af45edbc2d44e1f9f2267
Jason808 <78810864+JJJangJeongSu@users.noreply.github.com>
2024-07-29 14:53:41 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+  ll n;
+
+  cin >> n;
+
+  cout << n * (n - 1) * (n - 2) / 6 << '\n';
+
+  cout << 3;
+
+  return 0;
+}
