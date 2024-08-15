6ca59165675f5f333761c04ad669429b1a22b4cd
Jason808 <78810864+JJJangJeongSu@users.noreply.github.com>
2024-08-15 19:27:49 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
+++ C++17/백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -1,32 +1,33 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(NULL);
-  int arr[1000];
+  queue<int> q;
 
   int n, k;
   int count;
-  int cur = 0;
+  int temp;
+
   cin >> n >> k;
-
   for (int i = 0; i < n; i++)
-    arr[i] = i + 1;
-  cout << "<";
-  for (int total_count = 0; total_count < n; total_count++) {
-    count = 0;
-    while (count < k - 1 || arr[cur] == 0) {
-      if (arr[cur])
-        count++;
-      cur = (cur + 1) % n;
-    }
-    cout << arr[cur];
-    cout << (total_count == n - 1 ? ">" : ", ");
-    arr[cur] = 0;
-  }
+    q.push(i + 1);
 
-  cout << '\n';
+  cout << "<";
+  while (!q.empty()) {
+    count = 0;
+
+    for (int i = 0; i < k - 1; i++) {
+      temp = q.front();
+      q.pop();
+      q.push(temp);
+    }
+    cout << q.front();
+    q.pop();
+    cout << (q.empty() ? ">" : ", ");
+  }
 
   return 0;
 }
