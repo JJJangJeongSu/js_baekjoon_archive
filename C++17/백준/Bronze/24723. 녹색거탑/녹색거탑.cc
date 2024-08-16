356187cc46b16aa8a13bd3a7ecede6a950505cf7
Jason808 <78810864+JJJangJeongSu@users.noreply.github.com>
2024-08-16 18:05:40 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n;
+
+  cin >> n;
+
+  cout << pow(2, n) << '\n';
+
+  return 0;
+}
