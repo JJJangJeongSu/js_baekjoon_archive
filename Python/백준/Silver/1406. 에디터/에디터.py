5f45914555a17be4acb7d4824f1051169648901a
Jason808 <78810864+JJJangJeongSu@users.noreply.github.com>
2025-11-25 15:32:34 +0900

    [Silver II] Title: 에디터, Time: 440 ms, Memory: 37560 KB -BaekjoonHub

--- Python/백준/Silver/1406. 에디터/에디터.py
+++ Python/백준/Silver/1406. 에디터/에디터.py
@@ -1,83 +1,43 @@
-import sys
+import sys 
 
 input = sys.stdin.readline
 
-class Node:
-    def __init__(self, data):
-        self.prev = None 
-        self.next = None
-        self.data = data
-        
-class IDE:
-    def __init__(self):
-        self.begin_node = Node(data="BEGIN")
-        self.current_node = self.begin_node
-        
-    def print_all_node(self):
-        cur_node = self.begin_node.next
-        while not cur_node == None:
-            print(cur_node.data, end="") 
-            cur_node = cur_node.next
-    
-    def move_left(self):
-        if not self.current_node.prev == None:
-            self.current_node = self.current_node.prev
-        
-    def move_right(self):
-        if not self.current_node.next == None:
-            self.current_node = self.current_node.next
-            
-    def add_node(self, data):
-        new_node = Node(data=data)\
-    
-        new_node.next = self.current_node.next
-        
-        self.current_node.next = new_node
-        
-        if new_node.next != None:
-            new_node.next.prev = new_node
-        
-        new_node.prev = self.current_node
-        
-        self.current_node = new_node
-        
-    def delete_node(self):
-        if self.current_node.data == "BEGIN":
-            return
-        
-        self.temp_node = self.current_node
-        
-        self.current_node.prev.next = self.current_node.next
-        if self.current_node.next != None:
-            self.current_node.next.prev = self.current_node.prev
-        self.current_node = self.current_node.prev
-        
-        
+left_stack = []
+right_stack = []
 
-ide = IDE()
-
-
-input_string = input().strip()
-
-for letter in input_string:
-    ide.add_node(letter)
-
+init_string = input().strip()
 n = int(input())
+
+
+for letter in init_string:
+    left_stack.append(letter)
+    
+right_stack.reverse()
 
 for i in range(n):
     command = input().strip()
-    if command == 'L':
-        ide.move_left()
+    
+    if command == "L":
+        if left_stack:
+            right_stack.append(left_stack.pop())
     
     elif command == "D":
-        ide.move_right()
-        
+        if right_stack:
+            left_stack.append(right_stack.pop())
+            
     elif command == "B":
-        ide.delete_node()
+        if left_stack:
+            left_stack.pop()
+        
+    elif command[0] == "P":
+        left_stack.append(command[2])
+        
+        
+for c in left_stack:
+    print(c, end="")
     
-    elif command[0] == 'P':
-        ide.add_node(command[2]) 
-        
-        
-        
-ide.print_all_node()
+
+right_stack.reverse()
+
+for c in right_stack:
+    print(c, end="")
